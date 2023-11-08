2ee76ca443381b6c18609fd9a03314176800a7aa
Roberto-Waner <145742894+Roberto-Waner@users.noreply.github.com>
2023-11-08 18:03:32 -0400

    DiagramaFlujoEnC++

---
+++ EjercicioC++/Ejercicio3C++/EjercicioDF3C++.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int val1, val2, val3, medio, mayor, menor;
+	
+	cout <<"Ingrese el 1ro valor: ";
+	cin >> val1; cout <<endl;
+	
+	cout <<"Ingrese el 2do valor: ";
+	cin >> val2; cout <<endl;
+	
+	cout <<"Ingrese el 3ro valor: ";
+	cin >> val3; cout <<endl;
+	
+	if(mayor = (val1 > val2) || (val1 > val3)){
+		cout << "El numero" << mayor; cout <<"Es mayor" <<endl;
+		
+	}
+}
