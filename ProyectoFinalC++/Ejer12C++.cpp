5180ac1cff6ba0e9dcd20259b9cec6f7f5f34103
Roberto-Waner <145742894+Roberto-Waner@users.noreply.github.com>
2023-11-15 11:34:52 -0400

    The Final

---
+++ ProyectoFinalC++/Ejer12C++.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+
+using namespace std;
+
+float Suma (float v1, float v2, float v3);
+float impar (float v1, float v2, float v3);
+float par (float v1, float v2, float v3);
+float raiz2 (float v1, float v2, float v3);
+
+int main(){
+	
+}
+
+float Suma (float v1, float v2, float v3)
+{
+	float Suma = 0;
+	Suma = (v1 + v2 + v3);
+	
+	return Suma;
+}
+
+float impar (float v1, float v2, float v3)
+{
+	float impar = 0;
+	impar = (Suma %2 != 0);
+	
+	return impar;
+}
+
+float par (float v1, float v2, float v3)
+{
+	float par = 0;
+	par = (suma %2 == 0);
+	
+	return par;
+}
+
+float raiz2 (float v1, float v2, float v3)
+{
+	float raiz2 = 0;
+	raiz2 = pow()
+}
+
+
