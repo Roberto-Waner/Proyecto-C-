65ba5d62102d5cc8a60e3913774387bc17890137
Roberto-Waner <145742894+Roberto-Waner@users.noreply.github.com>
2023-11-22 15:23:06 -0400

    The Final

--- ProyectoFinalC++/EjerDF8C++.cpp
+++ ProyectoFinalC++/EjerDF8C++.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main(){
 	int num = 8, multi;
 	
-	cout <<"Tabla de multiplicacion del 8 hasta el valor 500\n\n";
+	cout <<"Tabla de multiplicacion del 8 que de como valor menor o igual a 500\n\n";
 	
-	for(int i = 1; i <= 500; i++){
+	for(int i = 1; i <= 62; i++){
 		multi = (num * i);
 		cout << num <<" x " << i << " = " << multi <<endl;
 	}
