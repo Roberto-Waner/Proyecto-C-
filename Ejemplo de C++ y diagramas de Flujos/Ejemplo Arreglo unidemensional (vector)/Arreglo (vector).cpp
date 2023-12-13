4bdcc220109892b18da123bb1e05ffc643da20c4
Roberto-Waner <145742894+Roberto-Waner@users.noreply.github.com>
2023-12-13 17:43:47 -0400

    Practica

--- Ejemplo de C++ y diagramas de Flujos/Ejemplo Arreglo unidemensional (vector)/Arreglo (vector).cpp
+++ Ejemplo de C++ y diagramas de Flujos/Ejemplo Arreglo unidemensional (vector)/Arreglo (vector).cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-//#include <cohio.h>
+#include <conio.h>
 
 using namespace std;
 
 int main (){
 	
-	int numero[], n;
+	int numero[100], n;
 	
 	cout<<"Entre cantidad de valor: ";
-	cin>> n <<endl;
+	cin>> n; cout<<endl;
 	
 	for(int z = 0; z < n; z++){
 		cout<<"Entre valor: ";
@@ -17,9 +17,40 @@ int main (){
 	
 	for(int z = 0; z < n; z++){
 		cout<<"Los numeros son: ";
-		cout<< numero[z];
+		cout<< numero[z]<<endl;
 	}
 	
-	//getch();
+	getch();
 	return 0;
 }
+
+/*
+#include <iostream>
+#include <conio.h>
+#include <vector>
+
+using namespace std;
+
+int main (){
+	
+	vector<int> numero;
+	int n;
+	
+	cout<<"Entre cantidad de valor: ";
+	cin>> n; cout<<endl;
+	
+	for(int z = 0; z < n; z++){
+		int temp;
+		cout<<"Entre valor: ";
+		cin>> temp;
+		numero.push_back(temp);
+	}
+	
+	for(int z = 0; z < n; z++){
+		cout<<"Los numeros son: ";
+		cout<< numero[z]<<endl;
+	}
+	
+	getch();
+	return 0;
+}*/
