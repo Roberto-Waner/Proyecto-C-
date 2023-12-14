043637895d8166d1da2f803e61cbe367c9925703
Roberto-Waner <145742894+Roberto-Waner@users.noreply.github.com>
2023-12-14 17:30:40 -0400

    arreglo

--- EjercicioArreglo/EjerArreglo.cpp
+++ EjercicioArreglo/EjerArreglo.cpp
@@ -14,45 +14,24 @@ int main (){
 	string calif;
 	
 	for(int i = 0; i < n; i++){
-		bool valid = false;
-		
-		while(!valid){
+	
 			cout<<"Ingrese la Nota #" << i+1 <<": ";
-			string temp;
-			cin>> temp; //cout<<endl;
-			//char str_temp[10];
-			//itoa(temp, str_temp, 10);
-			
-			try{
-				nota[i] = stoi(temp);
-				valid = true;
-				
-			}catch(exception &e){
-				cout<<"No se permiten Caractereres. Solo se aceptan numeros enteros como Nota.";
+			cin>> nota[i]; //cout<<endl;
+			if (nota[i] == false){
+				cout<<endl; cout<<"Valor de Nota incorecta. ";
+				system("pause");
+				cin.clear();
+				cin.ignore();
+				continue;
 			}
-			/*
-			for(int j = 0; j < 10; j++){
-				if(!isdigit(str_temp[j])){
-					cout<<"No se permiten Caractereres. Solo se aceptan numeros enteros como Nota.";
-					break;
-					
-				}else{
-					nota[i] = temp;
-					valid = true;
-				}
-			}*/
-			
-		}
-		
+	
 		
 		if(nota[i] < 0 || nota[i] > 100){
-			cout<<endl; cout<<"Valor de Nota incorecta. Las Notas deben de ser de 1 a 100\n\n";
+			cout<<endl; cout<<"Valor de Nota incorecta. Las Notas deben de ser de 1 a 100\n";
 			i--;
 		
 		}else if(nota[i] >= 90){   //Escala de calificaciones
 			Rg1++;
-			calif = "A";
-			cout <<"Pasa en " << calif <<". Esta Aprobado"<<endl <<endl;
 			
 		}else if(nota[i] >= 80 && nota[i] <= 89){
 			Rg2++;
@@ -81,12 +60,19 @@ int main (){
 	//Calculos de los promedios
 	prom = (float)suma /n;
 	
+	if(prom >= 90){
+		calif = "A";
+		cout <<"Pasa en " << calif <<". Esta Aprobado"<<endl <<endl;
+	}
+	
 	cout<<"Promedio de las Notas "<<prom<<endl;
+	
 	
 	cout <<"Estudiantes con Notas entre 1 a 69: " << Rg4 <<endl;
 	cout <<"Estudiantes con Notas entre 70 a 79: " << Rg3 <<endl;
 	cout <<"Estudiantes con Notas entre 80 a 89: " << Rg2 <<endl;
 	cout <<"Estudiantes con Notas entre 90 a 100: " << Rg1 <<endl;
+	
 	
 	getch();
 	return 0;
